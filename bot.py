ae15c917fa6831508e1f59e465bc8651f534b7c4
Tydus <Tydus@Tydus.org>
2017-04-25 00:43:25 +0900

    Init

---
+++ bot.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+import logging
+import urllib3
+import urllib
+import base64
+import json
+import md5
+import os
+import re
+import sys
+from lxml.etree import HTML
+import time
+from telegram import ChatAction, ParseMode, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler, Filters
+from telegram.ext.dispatcher import run_async
+
+t0 = time.time()
+from pred import predict
+print "Model prepared in %.2f s" % (time.time() - t0)
+
+http = urllib3.PoolManager()
+
+@run_async
+def image(bot, update):
+
+    #bot.sendChatAction(chat_id=update.message.chat_id, action=ChatAction.TYPING)
+
+    m = update.message
+    if m.photo:
+        file_obj = m.photo[-1]
+    elif m.document:
+        file_obj = m.document
+    else:
+        m.reply_text("Please send me image as photo or file.")
+        return 
+
+    photo_file = bot.getFile(file_obj.file_id)
+    data = http.request("GET", photo_file.file_path).data
+
+    id = md5.md5(data).hexdigest()
+
+    file("debug/%s.jpg" % id, "wb").write(data)
+
+    resp, result = predict("debug/%s.jpg" % id)
+    
+    reply_markup = InlineKeyboardMarkup([[
+        InlineKeyboardButton("⭕️", callback_data="%s/%d/Y" % (id, resp)),
+        InlineKeyboardButton("❓", callback_data="%s/%d/?" % (id, resp)),
+        InlineKeyboardButton("❌", callback_data="%s/%d/N" % (id, resp)),
+    ]])
+
+    m.reply_text(
+        "This image is a %d-jigen one. (S = %.4f)\n" % (resp, result) + 
+        "Please vote whether it is right.",
+        quote=True,
+        reply_markup=reply_markup,
+    )
+
+def feedback(bot, update):
+    m = update.callback_query
+
+    if not re.match(r"^[0-9A-Fa-f]{32}/[23]/[YN?]$", m.data):
+        m.answer() # Malformed data
+        return
+
+    id, jigen, answer = m.data.split('/')
+    try:
+        dirname = {"Y": "right", "N": "wrong", "?": "unknown"}[answer]
+    except:
+        m.answer() # Malformed data
+        return
+
+    if not os.path.isfile("debug/%s.jpg" % id):
+        m.answer("You already voted this image.")
+        return
+
+    os.rename("debug/%s.jpg" % id, "debug/%s/%s/%s.jpg" % (dirname, jigen, id))
+
+    m.answer("Voted %s." % dirname)
+    m.edit_message_reply_markup(reply_markup=None)
+
+def error(bot, update, error):
+    print error
+
+def main():
+    updater = Updater(sys.argv[1])
+
+    updater.dispatcher.add_handler(MessageHandler(
+        Filters.photo | 
+        Filters.document |
+
+        # These will be returned with error
+        Filters.text |
+        Filters.sticker
+    , image))
+
+    updater.dispatcher.add_handler(CallbackQueryHandler(feedback))
+    updater.start_polling()
+
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
---
+++ pred.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+from time import time
+from keras import applications
+from keras.preprocessing import image
+from keras import optimizers
+from keras.models import Sequential, Model
+from keras.layers import Dropout, Flatten, Dense
+from sys import argv
+from keras.applications.vgg16 import preprocess_input
+import math
+import numpy as np
+import tensorflow as tf
+
+
+# path to the model weights files.
+top_model_weights_path = 'bottleneck_fc_model.h5'
+# dimensions of our images.
+img_width, img_height = 224, 224
+
+# build the VGG16 network
+model = applications.VGG16(weights='imagenet', include_top=False, input_shape=(img_width, img_height, 3))
+
+# build a classifier model to put on top of the convolutional model
+top_model = Sequential()
+top_model.add(Flatten(input_shape=model.output_shape[1:]))
+top_model.add(Dense(256, activation='relu'))
+top_model.add(Dropout(0.5))
+top_model.add(Dense(1))
+
+top_model.load_weights(top_model_weights_path)
+
+# add the model on top of the convolutional base
+
+model = Sequential(layers=model.layers)
+model.add(top_model)
+
+#model.summary()
+
+
+# Save the TF graph to solve the multithread issues
+# See also: https://github.com/fchollet/keras/issues/2397
+graph = tf.get_default_graph()
+
+def predict(readable):
+    global model, graph
+    with graph.as_default():
+
+        img = image.load_img(readable, target_size=(img_height, img_width))
+        x = image.img_to_array(img)
+        x *= 1./255
+        x = np.expand_dims(x, axis=0)
+
+        try:
+            ret = model.predict(x)[0][0]
+
+            cat = 3 if ret > 0 else 2
+
+        except Exception, e:
+            print e.description
+
+        return cat, ret
+
